b35e329b39dccaf11e61fc4a51dce299e5c39bef
Andrea-Paperini <etabeta99@live.it>
2019-11-14 15:10:26 +0100

    Finito

---
+++ main.js
@@ -0,0 +1,9 @@
+var nome = prompt("Inserisci il tuo nome");
+console.log(nome);
+var cognome = prompt("Inserisci il tuo cognome")
+console.log(cognome);
+var colore = prompt("Inserisci il tuo colore preferito")
+console.log(colore);
+var password = nome + cognome + colore + "19";
+console.log(password);
+document.writeln(password);
